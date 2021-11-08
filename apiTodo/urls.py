97e80e3a4a2d930677e38afe4a9dfe14768db9b6
iycel <ismailycel@gmail.com>
2021-11-08 22:22:07 +0300

    feat: get and post

--- apiTodo/urls.py
+++ apiTodo/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import home, todoList, todoListCreate
+# from .views import todoList, todoListCreate
+from .views import home, toDo_list
 
 urlpatterns = [
     path('', home),
-    path('todolist/', todoList, name='todolist'),
-    path('todolistcreate/', todoListCreate, name='todolistcreate'),
+    # path('todolist/', todoList, name='todolist'),
+    # path('todolistcreate/', todoListCreate, name='todolistcreate'),
+    path('todolist/', toDo_list),
 ]
--- apiTodo/views.py
+++ apiTodo/views.py
@@ -1,9 +1,10 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
-from rest_framework import serializers
+from rest_framework import serializers, status
 from rest_framework.response import Response
 from .serializers import TodoSerializer
 from .models import Todo
+
 
 # Function Based Views
 from rest_framework.decorators import api_view
@@ -11,17 +12,31 @@ from rest_framework.decorators import api_view
 def home(request):
     return HttpResponse('<center><h1 style="background-image: linear-gradient(90deg, blue, turquoise); padding:10px;">Welcome to ApiTodo</h1></center>')
 
-@api_view(['GET'])
-def todoList(request):
-    queryset = Todo.objects.all()
+# @api_view(['GET'])
+# def todoList(request):
+#     queryset = Todo.objects.all()
 
-    serializer = TodoSerializer(queryset, many=True)
+#     serializer = TodoSerializer(queryset, many=True)
 
-    return Response(serializer.data)
+#     return Response(serializer.data)
 
-@api_view(['POST'])
-def todoListCreate(request):
-    serializer = TodoSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data)
+# @api_view(['POST'])
+# def todoListCreate(request):
+#     serializer = TodoSerializer(data=request.data)
+#     if serializer.is_valid():
+#         serializer.save()
+#     return Response(serializer.data)
+
+@api_view(['GET', 'POST'])
+def toDo_list(request):
+
+    if request.method == 'GET':
+        queryset = Todo.objects.all()
+        serializer = TodoSerializer(queryset, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_CREATED)
